b0933a883eabd3f62febc703faa2cdf39c198444
soet02 <alswnsrlf12@naver.com>
2024-11-02 17:52:49 +0900

    Feat: Home.js ( 메인 화면 ) 추가

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+// components/Home.js
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>메인 화면</h1>
+    </div>
+  );
+}
+
+export default Home;
