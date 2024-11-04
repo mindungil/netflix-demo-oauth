bb8c9fb2b08e589bb81bb269a02a7f5982bb8b10
soet02 <alswnsrlf12@naver.com>
2024-11-04 13:04:04 +0900

    Feat: Handle로직 수정

--- src/components/Auth/HandleAuth.js
+++ src/components/Auth/HandleAuth.js
@@ -3,12 +3,14 @@ import React, { useState } from 'react';
 import Signin from './Signin';
 import Register from './Register';
 import { CSSTransition, SwitchTransition } from 'react-transition-group';
-import './HandleAuth.css';
+import './HandleAuth.css'; // 애니메이션 효과를 위한 스타일 포함
 
 function HandleAuth() {
   const [showSignin, setShowSignin] = useState(true);
 
-  const toggleAuth = () => setShowSignin(!showSignin);
+  const toggleAuth = () => {
+    setShowSignin((prevShowSignin) => !prevShowSignin);
+  };
 
   return (
     <div className="auth-container">
