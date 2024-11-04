4ccc44375a8ccabe5402266b1a8ede5be12a56f3
soet02 <alswnsrlf12@naver.com>
2024-11-05 00:02:47 +0900

    Feat: config 추가

---
+++ src/components/config.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3';
+export const API_KEY = '611d06bda7fd5b6ba646060bd99fb648';
+export const EMAGE_BASE_URL = ''
