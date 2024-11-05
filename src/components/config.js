90f1a5b506c77db57d71f9e57007dea09971a0dc
soet02 <alswnsrlf12@naver.com>
2024-11-05 10:00:27 +0900

    Feat: config

--- src/components/config.js
+++ src/components/config.js
@@ -4,4 +4,4 @@ export const USER_SERVER = '/api/users';
 
 export const API_URL = 'https://api.themoviedb.org/3';
 export const API_KEY = '611d06bda7fd5b6ba646060bd99fb648';
-export const EMAGE_BASE_URL = ''
+export const EMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w1280/';
