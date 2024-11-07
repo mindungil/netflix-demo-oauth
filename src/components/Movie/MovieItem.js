83f265af57f28ae61fd2d2801d1e54c62154c1d3
soet02 <alswnsrlf12@naver.com>
2024-11-07 09:24:56 +0900

    Feat: Add vote-average

--- src/components/Movie/MovieItem.js
+++ src/components/Movie/MovieItem.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { EMAGE_URL_500 } from '../config';
+import { EMAGE_URL_300 } from '../config';
 
 const MovieItem = ({ movie }) => {
   return (
     <div className="movie-item">
       <img
-        src={EMAGE_URL_500 + movie.poster_path}
+        src={EMAGE_URL_300 + movie.poster_path}
         alt={movie.title}
       />
       <h3>{movie.title}</h3>
-      <p>{movie.vote_everage}</p>
+      <p>평점 : {movie.vote_average.toFixed(1)}</p>
       <p>{movie.release_date}</p>
     </div>
   );
