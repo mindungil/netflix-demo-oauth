fe47d97b3f6f12bd0e2d2152d739937421acc6fa
soet02 <alswnsrlf12@naver.com>
2024-11-07 16:39:08 +0900

    Feat: poster hover 추가

--- src/components/Movie/MovieItem.js
+++ src/components/Movie/MovieItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { EMAGE_URL_200 } from '../config';
+import './MovieList.css';
 
 const MovieItem = ({ movie }) => {
   return (
@@ -7,9 +8,10 @@ const MovieItem = ({ movie }) => {
       <img
         src={EMAGE_URL_200 + movie.poster_path}
         alt={movie.title}
+        className="zoom-in"
       />
       <h3>{movie.title}</h3>
-      <p>평점 : {movie.vote_average.toFixed(1)}</p>
+      <p style={{margin: 0}}>평점 : {movie.vote_average.toFixed(1)}</p>
       <p>{movie.release_date}</p>
     </div>
   );
