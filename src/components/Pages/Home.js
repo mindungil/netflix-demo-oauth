3ae02b4259b7835176c608676364de7bc39109d9
soet02 <alswnsrlf12@naver.com>
2024-11-12 11:40:26 +0900

    Feat: useEffect, timer 동작

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,23 +1,53 @@
-// components/Home.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css';
 import { EMAGE_BASE_URL_1280 } from '../config';
+import MovieItem from '../Movie/MovieItem';
+import { fetchTrends } from '../Movie/ApiRequest';
 
 function Home() {
-
+  const [trend, setTrend] = useState([]); // 빈 배열로 초기화
+  const [randomMovie, setRandomMovie] = useState(null); // null로 초기화
   
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const movies = await fetchTrends();
+        setTrend(movies);
+        if (movies.length > 0) {
+          setRandomMovie(movies[0]);
+        }
+      } catch (err) {
+        console.error('API 호출 실패: ', err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (trend.length > 0) { // trend가 비어 있지 않을 때만 실행
+      const intervalId = setTimeout(() => {
+        setRandomMovie(trend[getRandomMovie]);
+      }, 5000);
+
+      return () => clearTimeout(intervalId); // 컴포넌트가 unmount될 때 정리
+    }
+  }, [randomMovie]);
+
+  const getRandomMovie = () => Math.floor(Math.random() * 20);
+
   return (
     <div className="home">
-      <img 
-        src={EMAGE_BASE_URL_1280 + movie.poster_path}
-        art={movie.title}
-        className='main-poster'
+      {randomMovie && randomMovie.poster_path && ( // randomMovie가 null이 아니고 poster_path가 있을 때만 표시
+        <img 
+          src={EMAGE_BASE_URL_1280 + randomMovie.backdrop_path}
+          className="main-poster"
         />
-      {movies.map((movie) => {
-        <MovieItem movie={movie}/>
-      })}
-
-
+      )}
+      <br />
+{/* 
+      {trend.length > 0 && trend.map((movie) => ( // trend가 비어 있지 않을 때만 map 실행
+        <MovieItem key={movie.id} movie={movie} />
+      ))} */}
     </div>
   );
 }
