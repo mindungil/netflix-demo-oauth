af953e9eb32144ce4b4292a183168a1a3a9e6419
soet02 <alswnsrlf12@naver.com>
2024-11-14 14:49:15 +0900

    Feat: Toast-contanier 추가

--- src/components/CustomToast.js
+++ src/components/CustomToast.js
@@ -1,2 +1,30 @@
-import {toast} from 'react-toastify';
+import React from 'react';
+import {toast, ToastContainer} from 'react-toastify';
 
+export const successMessage = (message) => {
+    toast.success(message, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+        });
+}
+
+export const errorMessage = (message) => {
+    toast.error(message, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        transition: Bounce,
+        });
+}
