8f5c8309cb31dc60de9f02c73a066acb42454061
soet02 <alswnsrlf12@naver.com>
2024-11-18 10:49:50 +0900

    Feat: Auth 애니메이션 추가

--- src/components/Auth/HandleAuth.js
+++ src/components/Auth/HandleAuth.js
@@ -1,20 +1,48 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import Signin from "./Signin";
-import Register from './Register';
+import Register from "./Register";
 
 export function HandleAuth() {
-  const [isRegister, SetIsRegister] = useState(true);
+  const [isRegister, setIsRegister] = useState(true);
 
   const changeAuth = (state) => {
-    SetIsRegister(state);
-    console.log("isrender");
-    console.log(state);
+    setIsRegister(state);
   };
-  console.log("isRender");
+
+  const animationVariants = {
+    initial: { opacity: 0, rotateX: 80 },
+    animate: { opacity: 1, rotateX: 0 },
+    exit: { opacity: 0, rotateX: -80 },
+  };
 
   return (
-    <div>
-      {isRegister ? <Signin changeAuth={changeAuth} state={isRegister}/> : <Register changeAuth={changeAuth} state={isRegister}/>}
+    <div className="auth-container">
+      <AnimatePresence mode="wait">
+        {isRegister ? (
+          <motion.div
+            key="signin"
+            variants={animationVariants}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{ duration: 0.5 }}
+          >
+            <Signin changeAuth={changeAuth} state={isRegister} />
+          </motion.div>
+        ) : (
+          <motion.div
+            key="register"
+            variants={animationVariants}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{ duration: 0.5 }}
+          >
+            <Register changeAuth={changeAuth} state={isRegister} />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
