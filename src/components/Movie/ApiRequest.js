e95a0a7f9e14c52c6438ea9df97d3bf9a0b49c3b
soet02 <alswnsrlf12@naver.com>
2024-11-22 11:29:32 +0900

    Fix: API 호출 및 저장 로직 수정

--- src/components/Movie/ApiRequest.js
+++ src/components/Movie/ApiRequest.js
@@ -1,18 +1,23 @@
 import axios from 'axios';
+import { fetchApi } from '../../Util/config';
 
-const API_URL = process.env.REACT_APP_API_URL;
-const API_ACCESS = process.env.REACT_APP_API_ACCESS;
+// const API_URL = process.env.REACT_APP_API_URL;
+// const API_ACCESS = process.env.REACT_APP_API_ACCESS;
+
+export const API_URL = 'https://api.themoviedb.org/3'; 
 
 export const fetchMovies = async (page = 1) => {
   try {
+    const ApiData = await fetchApi();
     const res = await axios.get(`${API_URL}/movie/popular`, {
       params: {
         language: 'ko-KR',
         page,
+        api_key: ApiData,
       },
       headers: {
         accept: 'application/json',
-        Authorization: API_ACCESS,
+        // Authorization: API_ACCESS,
       },
     });
     return res.data.results;
@@ -24,13 +29,15 @@ export const fetchMovies = async (page = 1) => {
 
 export const fetchGenres = async () => {
   try {
+    const ApiData = await fetchApi();
     const res = await axios.get(`${API_URL}/genre/movie/list`, {
       params: {
         language: 'ko-KR',
+        api_key: ApiData,
       },
       headers: {
         accept: 'application/json',
-        Authorization: API_ACCESS,
+        // Authorization: API_ACCESS,
       }
     });
     return res.data.genres;
@@ -42,13 +49,15 @@ export const fetchGenres = async () => {
 
 export const fetchTrends = async () => {
   try {
+    const ApiData = await fetchApi();
     const res = await axios.get(`${API_URL}/trending/movie/week`, {
       params: {
         language: 'ko-KR',
+        api_key: ApiData,
       },
       headers: {
         accept: 'application/json',
-        Authorization: API_ACCESS,
+        // Authorization: API_ACCESS,
       }
     });
 
@@ -61,16 +70,18 @@ export const fetchTrends = async () => {
 
 export const searchMovies = async (name, page = 1) => {
   try {
+    const ApiData = await fetchApi();
     const res = await axios.get(`${API_URL}/search/movie`, {
       params: {
         query: name,
         include_adult: false,
         language: 'ko-KR',
         page, // 페이지 파라미터 추가
+        api_key: ApiData,
       },
       headers: {
         accept: 'application/json',
-        Authorization: API_ACCESS,
+        // Authorization: API_ACCESS,
       }
     });
     return res.data.results;
