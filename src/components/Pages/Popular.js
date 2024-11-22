c1f5896a49d0f8178f99b9ab62f8cb347e441cc8
soet02 <alswnsrlf12@naver.com>
2024-11-22 09:56:06 +0900

    Feat: fas icon 추가

--- src/components/Pages/Popular.js
+++ src/components/Pages/Popular.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react';
 import './Popular.css';
 import TableView from '../Movie/TableView';
 import ScrollView from '../Movie/ScrollView';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faTable, faInfinity } from '@fortawesome/free-solid-svg-icons';
 
 function Popular() {
   const [view, setView] = useState(true); // true: Table View, false: Scroll View
 
   return (
     <div className="popular">
-      <button onClick={() => setView(true)}>Table View</button>
-      <button onClick={() => setView(false)}>Scroll View</button>
+      <button onClick={() => setView(true)}><FontAwesomeIcon icon={faTable} className='fas'/></button>
+      <button onClick={() => setView(false)}><FontAwesomeIcon icon={faInfinity} className='fas'/></button>
       {view ? <TableView /> : <ScrollView />}
     </div>
   );
